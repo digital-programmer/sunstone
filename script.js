d5b57da337d8dbde335a486b1cf69c9a7977b3d7
digital-programmer <mohandibya123@gmail.com>
2022-01-19 10:25:24 +0530

    add responsive menu

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+
+function openNav() {
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mobile-menu").style.width = "0%";
+}
